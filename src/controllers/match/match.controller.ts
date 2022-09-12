30452fd00a53225f11a4554e3a1f192a6acf377a
fjrjdev <thedromemafia@gmail.com>
2022-09-12 18:56:09 -0300

    feat: match service import and route

--- src/controllers/match/match.controller.ts
+++ src/controllers/match/match.controller.ts
@@ -1,9 +1,10 @@
 import { Response, Request } from "express";
+import matchService from "../../services/match/match.service";
 
-export default async function matchController (req: Request, res: Response) {
+export default async function matchController(req: Request, res: Response) {
   const userId = req.params.id;
 
-  await (userId);
+  await matchService(userId);
 
   return res.json({ message: "Your email has been successfully confirmed" });
 }
