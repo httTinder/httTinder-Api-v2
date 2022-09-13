684c6a920c1a6bb8ab955be330b0e99eb43e3101
fjrjdev <thedromemafia@gmail.com>
2022-09-13 12:30:19 -0300

    feat email send

--- src/html/index.ts
+++ src/html/index.ts
@@ -1,16 +1,16 @@
-<!-- PRECISA DESCOBRIR COMO FAZER PARA CONECTAR AO EMAIL  -->
+import "dotenv/config";
 
+export function htmlBody(token: string) {
+  const link = process.env.BASE_URL;
+  const email = `
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-
      <head>
           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
           <title>Confirm Account KenzieLover!</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      </head>
-
      <body style="margin: 0; padding: 0; ">
-
           <!-- HEADER -->
           <table bgcolor="#D7234D" align="center" border="0" cellpadding="0" cellspacing="0" width="600">
                <tr>
@@ -32,7 +32,7 @@
                                    <td align="center" style="padding: 3em 0 1em;">
                                         <button style="background-color: #F299B9; border-radius: 10px; border: 0; padding: 1em 2em;  font-family: Arial, sans-serif; font-size: 1.5rem; ">
                                              <!-- precisa redirecionar para pagina com a rota + token -->
-                                             <a style="text-decoration: none; font-weight: 600; color: #ffffff;" href="https://htttinder.herokuapp.com/email/:tokenEmail" target="_blank">Confirm account</a>
+                                             <a style="text-decoration: none; font-weight: 600; color: #ffffff;" href="${link}${token}" target="_blank">Confirm account</a>
                                         </button>
                                    </td>
                               </tr>
@@ -43,7 +43,6 @@
                               </tr>
                               <!-- RODAPE -->
                               <tr>
-
                                    <td align="bottom" bgcolor="#D7234D" style="padding: 2em 2em 0;">
                                         <hr>
                                         <table border="0" cellpadding="0" cellspacing="0" width="100%">
@@ -56,12 +55,12 @@
                                                        </a>to this e-mail
                                                   </td>
                                              </tr>
-
-
                                         </table>
                                    </td>
                               </tr>
                          </table>
      </body>
+</html>`;
 
-</html>
+  return email;
+}
